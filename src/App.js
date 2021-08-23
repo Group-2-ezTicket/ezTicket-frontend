dfa0d64b76d6b7524ea8ffd5520855970533ab42
martier <martier@oocl.com>
2021-08-24 00:48:02 +0800

    [ST011][Front-end] feat: add select theatre and schedule buttons

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import React from 'react';
 import Footer from './components/Footer';
 import MenuHeader from "./components/MenuHeader";
 import './App.css';
-import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
       <div className="allButFooter">
       <MenuHeader/>
-      <div className="movieList">
-      <MovieList/>
-      </div>
       </div>
       <Footer/>
     </div>
