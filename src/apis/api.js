0188db88634b6eb24041fc3a7ce1e542f29e54f0
TORIBDA <david.moses.toribio@oocl.com>
2021-08-24 20:00:43 +0800

    test: delete branch later

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     //baseURL: 'http://localhost:8080'
-    baseURL:'https://group2-ita06-ezticket-backend.herokuapp.com/'
+    baseURL:'https://group2-ita06-ezticket-backend.herokuapp.com'
  });
  
 export default api;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Layout} from 'antd';
 import '../styles/Footer.css';
 
-function Footer(props) {
+function Footer() {
     return (
         <div>
             <Layout className="footer">EZTicket - ITA6 ©2021 Created by Group 2</Layout>
