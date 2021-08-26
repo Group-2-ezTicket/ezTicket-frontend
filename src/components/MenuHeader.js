5a8e715464393cc8421e7007b24971c837b11a9c
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-26 16:41:20 +0800

    [ST045][Front-end] feat: Added functionality for navigation links in menu header

--- src/components/MenuHeader.js
+++ src/components/MenuHeader.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
 import '../styles/MenuHeader.css'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import MovieView from './MovieView'
 import MovieList from './MovieList';
 import CheckoutSummary from './CheckoutSummary';
+import ReservationDetails from './ReservationDetails';
 
 const { Header } = Layout;
 
@@ -21,8 +22,8 @@ function MenuHeader(props) {
                                 defaultSelectedKeys={
                                     ['1']
                                 }>
-                                <Menu.Item key={1}>MOVIES</Menu.Item>
-                                <Menu.Item key={2}>FOOD OPTIONS</Menu.Item>
+                                <Menu.Item key={1}><Link to="/">MOVIES</Link></Menu.Item>
+                                <Menu.Item key={2}><Link to="/reservation">VIEW MY RESERVATION</Link></Menu.Item>
                             </Menu>
                         </div>
                     </Header>
@@ -33,6 +34,7 @@ function MenuHeader(props) {
                 <Route exact path="/movies/:id" component={MovieView}></Route>
                 {/* <Route exact path="/movies/*" render={props => <MovieView getCinemaId = {getCinemaId} />}></Route> */}
                 <Route exact path="/checkout" component={CheckoutSummary}></Route>
+                <Route exact path="/reservation" component={ReservationDetails}></Route>
             </Switch>
         </Router>
     );
